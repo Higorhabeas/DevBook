8c5defd8beccdc0d671174fa42e3b290280d6f1f
Higor <higoras.silva0@gmail.com>
2023-06-24 13:39:07 -0300

    Buscando vários usuários com like no nome ou nick e apenas um com ID

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -8,6 +8,10 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
@@ -45,11 +49,48 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando usuárioas!"))
+	nomeOunick := strings.ToLower(r.URL.Query().Get("usuario"))
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarios, erro := repositorio.Buscar(nomeOunick)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	respostas.Json(w, http.StatusOK, usuarios)
 }
 
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando um Usuário!"))
+	parametros := mux.Vars(r)
+
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuario, erro := repositorio.BuscarPorID(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.Json(w, http.StatusOK, usuario)
+
 }
 
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -3,6 +3,7 @@ package repositorios
 import (
 	"api/src/modelos"
 	"database/sql"
+	"fmt"
 )
 
 type usuarios struct {
@@ -29,4 +30,63 @@ func (repositorio usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 		return 0, erro
 	}
 	return uint64(ultimoIDInserido), nil
+}
+
+func (repositorio usuarios) Buscar(nomeOunick string) ([]modelos.Usuario, error) {
+	nomeOunick = fmt.Sprintf("%%%s%%", nomeOunick)
+
+	linhas, erro := repositorio.db.Query(
+		"select id, nome, nick, email, criadoem from usuarios where nome LIKE ? or nick LIKE ?",
+		nomeOunick, nomeOunick)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var umUsuario modelos.Usuario
+
+		if erro = linhas.Scan(
+			&umUsuario.ID,
+			&umUsuario.Nome,
+			&umUsuario.Nick,
+			&umUsuario.Email,
+			&umUsuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, umUsuario)
+	}
+
+	return usuarios, nil
+}
+
+func (repositorio usuarios) BuscarPorID(ID uint64) (modelos.Usuario, error) {
+	linha, erro := repositorio.db.Query(
+		"select id, nome, nick, email, criadoem from usuarios where id = ?",
+		ID,
+	)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linha.Close()
+
+	var umUsuario modelos.Usuario
+
+	if linha.Next() {
+		if erro = linha.Scan(
+			&umUsuario.ID,
+			&umUsuario.Nome,
+			&umUsuario.Nick,
+			&umUsuario.Email,
+			&umUsuario.CriadoEm,
+		); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+
+	}
+	return umUsuario, nil
 }
